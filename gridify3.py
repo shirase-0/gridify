856880635549a8306e7e550fab0c5b42636d6dd4
shirase-0 <ultra.gamer.ebf@gmail.com>
2022-03-03 20:44:15 +1100

    Quitting is the only option

--- gridify3.py
+++ gridify3.py
@@ -1,2 +1,15 @@
-print("Hello Sara!")
-print("Hello Tim!")
+#print("Hello Sara!")
+#print("Hello Tim!")
+
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+#reference = Image.open()
+
+window = tk.Tk()
+frame = ttk.Frame(window, padding=10)
+frame.grid()
+ttk.Button(frame, text="Quit", command=window.destroy).grid(column=1, row=0)
+
+window.mainloop()
