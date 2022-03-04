3b0fa62987ae9948388df90b8830d7143b857c97
dantessahh <saramcdonald22@yahoo.com.au>
2022-03-04 14:55:39 +1000

    experimenting with documentation code

--- gridify3.py
+++ gridify3.py
@@ -1,9 +1,10 @@
 #print("Hello Sara!")
 #print("Hello Tim!")
 
+import sys
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 
 window = Tk()
 window.title("Gridify")
@@ -16,7 +17,20 @@ def openfile():
 	reference = ImageTk.PhotoImage(Image.open(window.filename))
 	reference_label = Label(window, image=reference).pack()
 
+	with Image.open(window.filename) as im:
+		draw = ImageDraw.Draw(im)
+		draw.line((0, 0) + im.size, fill=128)
+		draw.line((0, im.size[1], im.size[0], 0), fill=128)
+	#write to stdout
+	im.save("test", "PNG")
+
 quitbutton = Button(window, text="Quit", command=window.destroy).pack()
 openbutton = Button(window, text="Open Image", command=openfile).pack()
 
+# get image size in width and height as a tuple
+#Image.size()
+
+
+
 window.mainloop()
+
